38253a37a6874cdf6c6563a8409c7586c6783fcd
Crisa Gazzola <gazcrisa@gmail.com>
2022-12-28 20:58:51 -0800

    Try out new design

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -3,6 +3,9 @@ import { Post } from "../../atoms/postsAtom";
 import { AiOutlineDelete } from "react-icons/ai";
 import { BsChat } from "react-icons/bs";
 import { BiUpvote, BiDownvote } from "react-icons/bi";
+import { IoBookmark } from "react-icons/io5";
+import { RiChat1Fill } from "react-icons/ri";
+import { BiLike } from "react-icons/bi";
 import { IoBookmarkOutline } from "react-icons/io5";
 import {
   Alert,
@@ -37,6 +40,25 @@ type PostItemProps = {
   onSelectPost?: (post: Post) => void;
   homePage?: boolean;
 };
+
+moment.locale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s",
+    s: "seconds",
+    ss: "%ss",
+    m: "1m",
+    mm: "%dm",
+    h: "1h",
+    hh: "%dh",
+    d: "1d",
+    dd: "%dd",
+    M: "a month",
+    MM: "%dM",
+    y: "1y",
+    yy: "%dY",
+  },
+});
 
 const PostItem: React.FC<PostItemProps> = ({
   post,
@@ -76,130 +98,137 @@ const PostItem: React.FC<PostItemProps> = ({
   };
 
   return (
-    <Flex
-      border="none"
-      bg="#222"
-      color="white"
-      borderRadius={singlePostPage ? "4px 4px 0px 0px" : "4px"}
-      _hover={{
-        borderColor: singlePostPage ? "none" : "gray",
-        transition: "700ms",
-      }}
-      cursor={singlePostPage ? "unset" : "pointer"}
-      onClick={() => onSelectPost && onSelectPost(post)}
-    >
+    <Flex direction="column" bg="#222">
       <Flex
-        direction="column"
-        align="center"
-        bg="#222"
-        borderRight="1px solid #444"
-        p={2}
-        width="40px"
-        borderRadius={singlePostPage ? "0" : "3px 0px 0px 3px"}
+        border="none"
+        color="white"
+        padding={"0px 10px 20px"}
+        borderRadius={singlePostPage ? "4px 4px 0px 0px" : "4px"}
+        _hover={{
+          borderColor: singlePostPage ? "none" : "gray",
+          transition: "700ms",
+        }}
+        cursor={singlePostPage ? "unset" : "pointer"}
+        onClick={() => onSelectPost && onSelectPost(post)}
       >
-        <Icon
-          as={BiUpvote}
-          color={userVoteValue === 1 ? "brand.100" : "rgb(129, 131, 132)"}
-          fontSize={26}
-          onClick={(event) => onVote(event, post, 1, post.id)}
-          cursor="pointer"
-        />
-        <Text fontSize="14pt">{post.voteStatus}</Text>
-        <Icon
-          as={BiDownvote}
-          color={userVoteValue === -1 ? "#9B2C2C" : "rgb(129, 131, 132)"}
-          fontSize={26}
-          onClick={(event) => onVote(event, post, -1, post.id)}
-          cursor="pointer"
-        />
-      </Flex>
-      <Flex direction="column" width="100%">
-        {error && (
-          <Alert status="error">
-            <AlertIcon />
-            <Text mr={2}>{error}</Text>
-          </Alert>
-        )}
-        <Stack spacing={3} p="10px">
-          <Stack
-            direction="row"
-            spacing={0.6}
-            align="center"
-            fontSize="12pt"
-            color="#777"
-          >
-            <Flex align="center" justifyContent="center">
-              <Text marginRight={1}>Posted by {post.creatorDisplayName}</Text>
-              <Text color={"rgb(129, 131, 132)"} fontSize="11pt">
-                {moment(new Date(post.createdAt?.seconds * 1000)).fromNow()}
-              </Text>
-            </Flex>
-          </Stack>
-          <Text fontSize="16pt" fontWeight={600}>
-            {post.title}
-          </Text>
-          <ReactQuill value={post.body} readOnly={true} theme={"bubble"} />
-          {post.imageURL && (
-            <Flex justify="center" align="center" p={2}>
-              {loadingImage && (
-                <Skeleton height="200px" width="100%" borderRadius={4} />
-              )}
-              <Image
-                src={post.imageURL}
-                maxHeight="460px"
-                alt="Post Image"
-                display={loadingImage ? "none" : "unset"}
-                onLoad={() => setLoadingImage(false)}
-              />
-            </Flex>
+        <Flex direction="column" width="100%">
+          {error && (
+            <Alert status="error">
+              <AlertIcon />
+              <Text mr={2}>{error}</Text>
+            </Alert>
           )}
-        </Stack>
-        <Flex ml={1} mb={0.5} color="gray.500">
-          <Flex
-            align="center"
-            p="8px 10px"
-            borderRadius={4}
-            _hover={{ bg: "rgba(102,122,128,0.10196078431372549)" }}
-            cursor="pointer"
-          >
-            <Icon as={BsChat} mr={1} fontSize="16pt" color="#777" />
-            <Text fontSize="11pt" fontWeight="700" color="#777">
-              {post.numberOfComments}
-            </Text>
-          </Flex>
-          <Flex
-            align="center"
-            p="8px 10px"
-            borderRadius={4}
-            _hover={{ bg: "rgba(102,122,128,0.10196078431372549)" }}
-            cursor="pointer"
-          >
-            <Icon as={IoBookmarkOutline} mr={1} fontSize="16pt" color="#777" />
-            <Text fontSize="11pt" fontWeight="700" color="#777">
-              Save
-            </Text>
-          </Flex>
-          {userIsCreator && (
+          <Stack spacing={3} p="10px">
+            <Stack
+              direction="row"
+              spacing={0.6}
+              align="center"
+              fontSize="12pt"
+              color="#777"
+            >
+              <Flex align="center" justifyContent="center">
+                <Text marginRight={1} fontSize="10pt">
+                  Posted by {post.creatorDisplayName}
+                </Text>
+              </Flex>
+            </Stack>
+            <Text fontSize="16pt">{post.title}</Text>
+            <ReactQuill value={post.body} readOnly={true} theme={"bubble"} />
+            {post.imageURL && (
+              <Flex justify="center" align="center" p={2}>
+                {loadingImage && (
+                  <Skeleton height="200px" width="100%" borderRadius={4} />
+                )}
+                <Image
+                  src={post.imageURL}
+                  maxHeight="460px"
+                  alt="Post Image"
+                  display={loadingImage ? "none" : "unset"}
+                  onLoad={() => setLoadingImage(false)}
+                />
+              </Flex>
+            )}
+          </Stack>
+        </Flex>
+      </Flex>
+      <Flex borderTop="1px solid" borderColor="#444" padding="0px 0px 0px 0px">
+        <Flex
+          align="center"
+          justifyContent="space-between"
+          padding={"8px 10px"}
+          width="100%"
+        >
+          <Stack direction="row">
             <Flex
               align="center"
               p="8px 10px"
               borderRadius={4}
               _hover={{ bg: "rgba(102,122,128,0.10196078431372549)" }}
               cursor="pointer"
-              onClick={handleDelete}
             >
-              {loadingDelete ? (
-                <Spinner size="sm" />
-              ) : (
-                <>
-                  <Icon as={AiOutlineDelete} mr={1} fontSize="16pt" color="#777" />
-                  <Text fontSize="11pt" fontWeight="700" color="#777">
-                    Delete
-                  </Text>
-                </>
-              )}
+              <Icon
+                as={BiLike}
+                mr={1}
+                fontSize="15pt"
+                color={userVoteValue === 1 ? "brand.100" : "rgb(129, 131, 132)"}
+                onClick={(event) => onVote(event, post, 1, post.id)}
+              />
+              <Text fontSize={{ base: "10pt", sm: "11pt" }} color="#777">
+                {post.numberOfComments > 0 ? post.voteStatus : "Like"}
+              </Text>
             </Flex>
-          )}
+            <Flex
+              align="center"
+              p="8px 10px"
+              borderRadius={4}
+              _hover={{ bg: "rgba(102,122,128,0.10196078431372549)" }}
+              cursor="pointer"
+            >
+              <Icon as={RiChat1Fill} mr={1} fontSize="16pt" color="#777" />
+              <Text fontSize={{ base: "10pt", sm: "11pt" }} color="#777">
+                {post.numberOfComments > 0 ? post.numberOfComments : "Comment"}
+              </Text>
+            </Flex>
+            {userIsCreator && (
+              <Flex
+                align="center"
+                p="8px 10px"
+                borderRadius={4}
+                _hover={{ bg: "rgba(102,122,128,0.10196078431372549)" }}
+                cursor="pointer"
+                onClick={handleDelete}
+              >
+                {loadingDelete ? (
+                  <Spinner size="sm" />
+                ) : (
+                  <>
+                    <Icon
+                      as={AiOutlineDelete}
+                      mr={1}
+                      fontSize="16pt"
+                      color="#777"
+                    />
+                    <Text fontSize="11pt" fontWeight="700" color="#777">
+                      Delete
+                    </Text>
+                  </>
+                )}
+              </Flex>
+            )}
+          </Stack>
+          <Text color={"#777"} fontSize={{ base: "10pt", sm: "11pt" }}>
+            {moment(new Date(post.createdAt?.seconds * 1000)).fromNow()}
+          </Text>
+        </Flex>
+        <Flex
+          align="center"
+          p="8px 10px"
+          borderLeft="1px solid"
+          borderColor="#444"
+          _hover={{ bg: "rgba(102,122,128,0.10196078431372549)" }}
+          cursor="pointer"
+        >
+          <Icon as={IoBookmark} mr={1} fontSize="15pt" color="#777" />
         </Flex>
       </Flex>
     </Flex>
